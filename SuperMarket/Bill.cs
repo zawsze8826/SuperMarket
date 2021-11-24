80f39c87949c605d69a34da34fb09de5f187c3d5
dqtuan8826@gmail.com <dqtuan8826@gmail.com>
2021-11-24 17:05:00 +0700

    add thêm diagram

--- SuperMarket/Bill.cs
+++ SuperMarket/Bill.cs
@@ -6,52 +6,53 @@ using System.Threading.Tasks;
 
 namespace SuperMarket
 {
-    class Bill 
+    class Bill
     {
-    //    private int quantytiPurchase;
-    //    private double vat;
-    //    private double totalPrice;
-    //    private List<Bill> bill = new List<Bill>();
-    //    public int QuantytiPurchase { get => quantytiPurchase; set => quantytiPurchase = value; }
-    //    public double Vat { get => vat; set => vat = value; }
-    //    public double TotalPrice { get => totalPrice; set => totalPrice = value; }
+        //    private int quantytiPurchase;
+        //    private double vat;
+        //    private double totalPrice;
+        //    private List<Bill> bill = new List<Bill>();
+        //    public int QuantytiPurchase { get => quantytiPurchase; set => quantytiPurchase = value; }
+        //    public double Vat { get => vat; set => vat = value; }
+        //    public double TotalPrice { get => totalPrice; set => totalPrice = value; }
 
-    //    public Bill()
-    //    { }
-    //    ~Bill()
-    //    { }
-    //    public void AddBill()
-    //    {
-    //        Bill go = new Bill();
-    //        Console.Write("Nhap ID san pham:");
-    //        string idgoods = Console.ReadLine();
-    //        Console.Write("Nhap so luong mua:");
-    //        int quan = Convert.ToInt32(Console.ReadLine());
+        //    public Bill()
+        //    { }
+        //    ~Bill()
+        //    { }
+        //    public void AddBill()
+        //    {
+        //        Bill go = new Bill();
+        //        Console.Write("Nhap ID san pham:");
+        //        string idgoods = Console.ReadLine();
+        //        Console.Write("Nhap so luong mua:");
+        //        int quan = Convert.ToInt32(Console.ReadLine());
 
-    //        foreach(Goods g in SuperMarket.InventorieList)
-    //        {
-    //            if (g.IDGoods == idgoods)
-    //            {
-    //                go.IDGoods = g.IDGoods;
-    //                go.NameGoods = g.NameGoods;
-    //                go.QuantytiPurchase = quan;
-    //                go.Price = g.Price*quan;
-    //                go.Vat = (double)go.Price * 10 / (double)100;
-    //                bill.Add(go);
-    //            }
-    //        }
-    //    }
-    //    public void DisplayBillPurchase()
-    //    { }
-    //    public void DisplayBillList()
-    //    {
-    //        foreach (List<Bill> l in SuperMarket.BillList)
-    //        {
-    //            foreach(Bill b in l)
-    //            {
-                    
-    //            }
-    //        }
-    //    }
-    //}
+        //        foreach(Goods g in SuperMarket.InventorieList)
+        //        {
+        //            if (g.IDGoods == idgoods)
+        //            {
+        //                go.IDGoods = g.IDGoods;
+        //                go.NameGoods = g.NameGoods;
+        //                go.QuantytiPurchase = quan;
+        //                go.Price = g.Price*quan;
+        //                go.Vat = (double)go.Price * 10 / (double)100;
+        //                bill.Add(go);
+        //            }
+        //        }
+        //    }
+        //    public void DisplayBillPurchase()
+        //    { }
+        //    public void DisplayBillList()
+        //    {
+        //        foreach (List<Bill> l in SuperMarket.BillList)
+        //        {
+        //            foreach(Bill b in l)
+        //            {
+
+        //            }
+        //        }
+        //    }
+        //}
+    }
 }
